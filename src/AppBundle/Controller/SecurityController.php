9dfb8dc6cbb668a82a46b99256d9d17ac35213aa
Eronne <erwann.letue@gmail.com>
2017-06-18 17:53:31 -0400

    Translation of the login page

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 class SecurityController extends \FOS\UserBundle\Controller\SecurityController
 {
     /**
+     * @Route("/login/{_locale}", name="login", defaults={"_locale": "fr"}, requirements={
+     *     "_locale": "fr|en"
+     * })
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
