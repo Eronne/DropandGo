2b9ea73f6c5e2e49ab8507d52e66f712f58b8990
Eronne <erwann.letue@gmail.com>
2017-06-19 18:45:48 -0400

    Hided languages button on users pages

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -2,11 +2,18 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationController extends \FOS\UserBundle\Controller\RegistrationController
 {
+    /**
+     * @Route("/login/{_locale}", name="register", defaults={"_locale": "fr"}, requirements={
+     *     "_locale": "fr|en"
+     * })
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function registerAction(Request $request)
     {
         if ($this->getUser()) return $this->redirectToRoute('homepage');
